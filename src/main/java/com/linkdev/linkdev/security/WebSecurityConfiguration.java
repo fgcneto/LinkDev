5c518dbcdfcd92c4dc91bd125ff0bf34aae1513c
fgcneto <fgcneto@gmail.com>
2022-05-18 19:56:48 -0300

    correção logout

--- src/main/java/com/linkdev/linkdev/security/WebSecurityConfiguration.java
+++ src/main/java/com/linkdev/linkdev/security/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.linkdev.linkdev.security;
 
+import ch.qos.logback.classic.Logger;
 import com.linkdev.linkdev.repository.UserRepository;
 import com.linkdev.linkdev.services.MyUserDatailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +8,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.core.userdetails.UserDetailsService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
 
     @Bean
     public static BCryptPasswordEncoder passwordEncoder(){
@@ -37,11 +40,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers( "/h2-console/**","/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login")
-                .failureUrl("/login") // página de destino após um login malsucedido
-                .permitAll();
+                .failureUrl("/login?logout") // redireciona para login
+                .permitAll()
+                .and()
+                .csrf().disable();
+                http.headers().frameOptions().disable();
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
